29c9b3547a4e31020df0cf9a77a46172ceb1f2ad
Sarah Marquardt <smarquardtcwp@gmail.com>
2019-03-04 13:50:00 -0600

    fixes

--- static/js/app.js
+++ static/js/app.js
@@ -4,25 +4,21 @@ function buildMetadata(sample) {
 
   // Use `d3.json` to fetch the metadata for a sample
     // Use d3 to select the panel with id of `#sample-metadata`
-    var url = `/metadata/${sample}`;
-    d3.json(url).then(function(sample){
+    var metadataURL = `metadata/${sample}`;
+    panelMetaData = d3.select("#sample-metadata");
 
-         // Use `.html("") to clear any existing metadata
-    sample_metadata.html("");    
+   // Use `.html("") to clear any existing metadata
+    panelMetaData.html("");    
     
+  // Use `Object.entries` to add each key and value pair to the panel
+  // Hint: Inside the loop, you will need to use d3 to append new
+  // tags for each key-value in the metadata.
 
- 
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
-
-    Object.defineProperties(sample).forEach(function([key, value]) {
-      var row = sample_metadata.append("p");
-      row.text(`${key}: ${value}`);
+  d3.json(metadataURL).then(function (data) {
+    Object.entries(data).forEach(([key, value]) => {
+      panelMetadata.append("h6").text(`${key}: ${value}`);
     })
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
-  });
+  })
 }
 
 function buildCharts(sample) {
